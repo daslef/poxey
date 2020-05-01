5b3f4df62673d89f8c8de95577f4cd9eaf3b16cc
Zanor <pavlov.yaroslaw@gmail.com>
2020-05-01 18:38:24 +0300

    session add

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import timedelta
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,26 +6,30 @@ app = Flask(__name__)
 app.secret_key = '*PInefdlyv5@'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.permanent_session_lifetime = timedelta(hours=4)
 
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     email = db.Column(db.String(100))
     password = db.Column(db.String(100))
-    
+
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
-        
+
     def __str__(self):
         return f"Username - {self.name} Email - {self.email}"
-    
+
 
 @app.route('/')
 def index():
+    if session:
+        return redirect(url_for("search"))
     return render_template("index.html")
 
 
@@ -36,17 +40,19 @@ def signup():
         email = request.form['email']
         password = request.form['password']
         found_user = User.query.filter_by(name=username).first()
-        
+
         if found_user:
             print(found_user)
         else:
             user = User(username, email, password)
             db.session.add(user)
             db.session.commit()
+            session['username'] = username
         return redirect(url_for("search"))
-        
+
     return render_template("signup.html")
-    
+
+
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
     if request.method == "POST":
@@ -54,6 +60,7 @@ def signin():
         password = request.form['password']
         user = User.query.filter_by(name=username).first()
         if user and user.password == password:
+            session['username'] = username
             return redirect(url_for('search', name=user.name))
     return render_template("signin.html")
 
@@ -62,13 +69,16 @@ def signin():
 def profile(name):
     return render_template("profile.html")
 
+
 @app.route('/adventure', methods=['GET', 'POST'])
 def adventure():
     return render_template("adventure.html")
-    
+
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     return render_template("search.html")
+
 
 if __name__ == "__main__":
     db.create_all()
